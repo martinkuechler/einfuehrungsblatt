f25d8511308b2bbe324f7aa54a15b392f7500b72
martin <martinnice1379@gmail.com>
2025-10-28 11:51:40 +0100

    frontend implemented and backend connected

--- backend/api.py
+++ backend/api.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 import datetime
 
 app = FastAPI()
 
-
 @app.get("/")
-async def root() -> str:
-    return "You can visit YourLocalIP/api/ping or YourLocalIP/api/pong"
-
+def index():
+    return FileResponse("frontend/index.html")
 
 @app.get("/api/ping")
 async def ping() -> dict:
@@ -17,5 +16,7 @@ async def ping() -> dict:
 
 @app.post("/api/pong")
 async def pong() -> str:
-    return "Pong"
+        print("pong received")   # <-- loggt ins Server-Log
+        return ""
+        
 
