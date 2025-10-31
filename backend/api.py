7af4a82e89a0dbd5cbc40e74615d7c93bd2f111f
martin <martinnice1379@gmail.com>
2025-10-31 07:51:04 +0100

    Basic todo implemented

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 import datetime
+import sqlite3
+
 
 app = FastAPI()
+con=sqlite3.connect('todo.db')
+cur = con.cursor()
+
+con.commit()
+
+try:
+    cur.execute("""
+    CREATE TABLE todo(
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        content TEXT NOT NULL,
+        created_at TEXT NOT NULL
+    )
+""")
+    con.commit()
+
+except:
+    pass
+print(cur.execute("SELECT * FROM todo").fetchall())
+
+
+
 
 @app.get("/")
 def index():
@@ -16,7 +40,54 @@ async def ping() -> dict:
 
 @app.post("/api/pong")
 async def pong() -> str:
-        print("pong received")   # <-- loggt ins Server-Log
+        print("pong")   # <-- loggt ins Server-Log
         return ""
         
+#aufgabe 2 ab hier
+@app.post("/api/items")
+async def create_item(request: Request):
+     body=await request.json()
+     body["id"] = int(body["id"])
 
+     print(body)
+     if "id" not in body or "title" not in body or "content" not in body:
+         print("Fehlende Parameter")
+         return "Fehlende Parameter"
+     if cur.execute("SELECT * FROM todo WHERE id=?",(body["id"],)).fetchone() is not None:
+         return "Eintrag mit dieser ID existiert bereits"
+
+     res=cur.execute("INSERT INTO todo VALUES (?,?,?,?)",(body["id"],body['title'],body['content'],datetime.datetime.now().isoformat()))
+     print(res)
+     con.commit()
+
+     return ""
+
+@app.get("/api/items")
+async def get_items():
+        items=cur.execute("SELECT * FROM todo").fetchall()
+        result=[]
+        for item in items:
+            result.append({
+                "id":item[0],
+                "title":item[1],
+                "content":item[2],
+                "created_at":item[3]
+            })
+        return result
+
+@app.get("/api/items/{id}")
+async def get_item(id):
+     pass
+
+@app.delete("/api/items/{id}")
+async def delete_item(id: int ):
+     print("delete requested")
+     cur.execute("DELETE FROM todo WHERE id=?",(id,))
+     print("rows deleted:", cur.rowcount)
+     con.commit()
+     return ""
+    
+     
+
+
+     
